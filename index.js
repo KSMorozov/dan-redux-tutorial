b7a5bd2e7038cf35d367e6d312900a37aaaedd02
Kirill Morozov <tiguhrrr@gmail.com>
2016-04-03 21:28:51 +0300

    Add Avoiding Mutations.

--- index.js
+++ index.js
@@ -1,42 +1,43 @@
-import React, { PropTypes } from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import expect from 'expect';
+import deepFreeze from 'deep-freeze';
 
-const counter = (state = 0, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
-    default:
-      return state;
-  }
+const addCounter = (list) => [...list, 0];
+
+const removeCounter = (list, index) => [...list.slice(0, index), ...list.slice(index + 1)];
+
+const testAddCounter = () => {
+  const listBefore = [];
+  const listAfter = [0];
+
+  deepFreeze(listBefore);
+
+  expect(addCounter(listBefore))
+    .toEqual(listAfter);
 };
 
-const store = createStore(counter);
-const Counter = ({ value, onIncrement, onDecrement }) =>
-  <div>
-    <h1>{value}</h1>
-    <button onClick={onIncrement}>+</button>
-    <button onClick={onDecrement}>-</button>
-  </div>;
+const incrementCounter = (list, index) => list.map((e, i) => (i === index ? e + 1 : e));
 
-Counter.propTypes = {
-  value: PropTypes.number.isRequired,
-  onIncrement: PropTypes.func.isRequired,
-  onDecrement: PropTypes.func.isRequired,
+const testRemoveCounter = () => {
+  const listBefore = [0, 10, 20];
+  const listAfter = [0, 20];
+
+  deepFreeze(listBefore);
+
+  expect(removeCounter(listBefore, 1))
+    .toEqual(listAfter);
 };
 
-const render = () => {
-  ReactDOM.render(
-    <Counter
-      value={store.getState()}
-      onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
-      onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
-    />,
-    document.getElementById('root')
-  );
+const testIncrementCounter = () => {
+  const listBefore = [0, 10, 20];
+  const listAfter = [0, 11, 20];
+
+  deepFreeze(listBefore);
+
+  expect(incrementCounter(listBefore, 1))
+    .toEqual(listAfter);
 };
 
-store.subscribe(render);
-render();
+testAddCounter();
+testRemoveCounter();
+testIncrementCounter();
+console.log('All tests passed.');
